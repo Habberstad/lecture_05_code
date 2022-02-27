80b0e85e294ce1f191519ce5a9d87dee5f077592
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-02-27 19:11:18 +0100

    testing husky precommit

--- server/server.js
+++ server/server.js
@@ -14,5 +14,7 @@ app.post("/login", (req, res) => {
 
 
 const server = app.listen(process.env.PORT || 3000, ()=> {
+
     console.log(`Server listening on http://localhost:${server.address().port}`)
+
 })
