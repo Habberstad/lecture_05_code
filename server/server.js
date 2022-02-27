be46389d65dbea3506dc5063104a69f1c12c556d
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-02-27 20:24:19 +0100

    simple react app

--- server/server.js
+++ server/server.js
@@ -4,14 +4,16 @@ const app = express();
 
 // path = /login
 // handle login logic
-app.get("/login", (req, res) => {
+app.get("/login", (req, res, next) => {
   res.json({ username: "Noen Andre Persson" });
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", (req, res, next) => {
   res.sendStatus(401);
 });
 
+app.use(express.static("public"));
+
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server listening on http://localhost:${server.address().port}`)
+  console.log(`Server listening on http://localhost:${server.address().port}`);
 });
