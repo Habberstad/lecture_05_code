607acdc63619db8e0d6e58f0ca35fe06f7709d0f
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-02-27 18:20:49 +0100

    Added port for Heroku

--- server/server.js
+++ server/server.js
@@ -13,4 +13,6 @@ app.post("/login", (req, res) => {
 })
 
 
-app.listen(3000)
+const server = app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`Server listening on http://localhost:${server.address().port}`)
+})
