59f0f402426fc35b9cd42c67db296a52698ef8d7
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-02-28 20:09:36 +0100

    Added middleware and new pages

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(
-  <h1>Hello test test express</h1>,
-  document.getElementById("app")
-);
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movie database</h1>
+      <ul>
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          Username
+          <input type="text" />
+        </div>
+        <div>
+          Password
+          <input type="password" />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/*"} element={<h1>Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -1,18 +1,26 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
 // path = /login
 // handle login logic
-app.get("/login", (req, res, next) => {
+app.get("/api/login", (req, res, next) => {
   res.json({ username: "Noen Andre Persson" });
 });
 
-app.post("/login", (req, res, next) => {
+app.post("/api/login", (req, res, next) => {
   res.sendStatus(401);
 });
 
 app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening on http://localhost:${server.address().port}`);
